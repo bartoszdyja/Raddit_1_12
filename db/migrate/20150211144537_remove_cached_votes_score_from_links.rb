669bd5dfabf2d3fc7de2f08b3076b1810240db76
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-02-11 16:52:35 +0100

    Removed migration

--- db/migrate/20150211144537_remove_cached_votes_score_from_links.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCachedVotesScoreFromLinks < ActiveRecord::Migration
-    def up
-    remove_column :links, :cached_votes_score
-  end
-
-  def down
-    add_column :link, :cached_votes_score, :integer
-  end
-end
